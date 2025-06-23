e98c1f70b1e62b730e42f0abb03cee4984061914
Seya <31957516+sglkc@users.noreply.github.com>
2025-06-23 14:59:50 +0700

    feat: init golang template

---
+++ apps/zog-news/README.md
@@ -0,0 +1,101 @@
+# Golang Project Template
+Basic Go application for backend
+
+### Prerequisites
+
+- Golang v1.24
+- [Moonrepo](https://moonrepo.dev/docs/getting-started/installation)
+- Postgres v17.x.x
+- Docker and Docker Compose (for containerized development)
+
+### Quick Start
+
+#### Create New Application
+
+1. Generate a new application from a template.
+
+```bash
+moon generate template-golang
+```
+
+2. Set up environment variables:
+```bash
+cp .env.example .env
+```
+Adjust the variable to the desired value.
+
+3. Install dependencies:
+```bash
+moon run tidy
+```
+
+#### Running The Application
+
+1. Running on development mode
+```bash
+moon run dev
+```
+
+2. Build the application
+```bash
+moon run build
+```
+
+3. Running on production mode
+```bash
+moon run start
+```
+
+#### Running Migration
+
+1. Create new migration file
+```bash
+moon run migration-create -- {migration_name}
+```
+
+2. Migration up
+```bash
+moon run migration-up
+```
+
+3. Migration down
+```bash
+moon run migration-down
+```
+
+4. Migration reset
+```bash
+moon run migration-reset
+```
+
+4. Check Migration version
+```bash
+moon run migration-version
+```
+
+#### Running Seeders
+
+1. Run seeders for all tables
+```bash
+moon run seed -- all
+```
+
+2. Run seeder for certain table
+```bash
+moon run seed -- {table_name}
+```
+#### Running Tests
+
+```bash
+moon run zog-news:test
+```
+
+## Production
+
+### Instrumentation
+Tracing is enabled exclusively in the production environment. Set `APP_ENVIRONMENT` to `production` to activate tracing. Alternatively, you may customize the tracing rules in `apps/zog-news/config/tracer.go`.
+
+For instructions on customizing span tracing, please refer to the example located at:
+- `apps/zog-news/internal/rest/user.go`
+    - From rest layer all the way down to repository layer
+
---
+++ apps/zog-news/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"zog-news/config"
+	"zog-news/database"
+	"zog-news/domain"
+	"zog-news/internal/repository/postgres"
+	"zog-news/internal/rest"
+	"zog-news/internal/rest/middleware"
+	"zog-news/service"
+
+	"github.com/labstack/echo/v4"
+	"github.com/lmittmann/tint"
+)
+
+func init() {
+	config.LoadEnv()
+}
+
+func main() {
+
+	env := os.Getenv("APP_ENVIRONMENT")
+	var handler slog.Handler
+
+	w := os.Stdout
+	if env == "local" {
+		handler = tint.NewHandler(w, &tint.Options{
+			ReplaceAttr: middleware.ColorizeLogging,
+		})
+	} else {
+		// or continue setup log for another env
+		handler = slog.NewTextHandler(w, nil)
+	}
+
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+
+	dbPool, err := database.SetupPgxPool()
+	if err != nil {
+		slog.Error("Failed to set up database", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+	defer dbPool.Close()
+
+	e := echo.New()
+	e.HideBanner = true
+
+	e.Logger.SetOutput(os.Stdout)
+	e.Logger.SetLevel(0)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	tp, shutdown := config.InitTracer(ctx)
+	defer shutdown(ctx)
+
+	e.Use(middleware.AttachTraceProvider(tp))
+	e.Use(middleware.SlogLoggerMiddleware())
+	e.Use(middleware.Cors())
+
+	// Register the routes
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, domain.Response{
+			Code:    200,
+			Status:  "Succes",
+			Message: "All is well!",
+		})
+	})
+
+	userRepo := postgres.NewUserRepository(dbPool)
+	userService := service.NewUserService(userRepo)
+
+	apiV1 := e.Group("/api/v1")
+	usersGroup := apiV1.Group("")
+
+	rest.NewUserHandler(usersGroup, userService)
+
+	// Get host from environment variable, default to 127.0.0.1 if not set
+	host := os.Getenv("APP_HOST")
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
+	// Get port from environment variable, default to 8000 if not set
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	// Server address and port to listen on
+	serverAddr := fmt.Sprintf("%s:%s", host, port)
+
+	go func() {
+		slog.Info("Server starting", "address", serverAddr)
+		if err := e.Start(serverAddr); err != nil && err != http.ErrServerClosed {
+			slog.Error("Server failed", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	slog.Info("Shutting down server gracefully...")
+	if err := e.Shutdown(ctx); err != nil {
+		slog.Error("Shutdown error", "error", err)
+	}
+}
