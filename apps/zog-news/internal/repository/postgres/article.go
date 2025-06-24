828c499e56c9c2e7a162c3d190bbc9763851e2b6
Seya <31957516+sglkc@users.noreply.github.com>
2025-06-24 10:44:07 +0700

    feat(internal): add article repository & service
    feat(domain): add news not found error

---
+++ apps/zog-news/internal/repository/postgres/article.go
@@ -0,0 +1,164 @@
+package postgres
+
+import (
+	"context"
+	"strings"
+	"zog-news/domain"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+)
+
+type ArticleRepository struct {
+	Conn *pgxpool.Pool
+}
+
+func NewArticleRepository(conn *pgxpool.Pool) *ArticleRepository {
+	return &ArticleRepository{
+		Conn: conn,
+	}
+}
+
+func (a *ArticleRepository) CreateArticle(ctx context.Context, article *domain.CreateArticleRequest) (*domain.Article, error) {
+
+	query := `
+		INSERT INTO articles (title, content, author, status, created_at, updated_at)
+		VALUES ($1, $2, $3, NOW(), NOW())
+		RETURNING id`
+
+	var id uuid.UUID
+	var err = a.Conn.QueryRow(ctx, query, article.Title, article.Content, article.Author, article.Status).Scan(&id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &domain.Article{
+		ID:    id.String(),
+		Title:  article.Title,
+		Author: article.Author,
+		Content:  article.Content,
+		Status: article.Status,
+	}, nil
+}
+
+func (a *ArticleRepository) GetArticleList(ctx context.Context, filter *domain.ArticleFilter) ([]domain.Article, error) {
+	query := `
+		SELECT
+			a.id,
+			a.title,
+			a.content,
+            a.created_at,
+            a.updated_at
+		FROM articles u
+        WHERE a.deleted_at is NULL`
+
+	var args []interface{}
+	var conditions []string
+	if filter != nil && filter.Search != "" {
+		conditions = append(conditions, `(a.title ILIKE $1 OR u.content ILIKE $1)`)
+		args = append(args, "%"+filter.Search+"%")
+	}
+
+	if len(conditions) > 0 {
+		query += strings.Join(conditions, " AND ")
+	}
+	rows, err := a.Conn.Query(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var articles []domain.Article
+
+	for rows.Next() {
+		var article domain.Article
+		err := rows.Scan(
+			&article.ID,
+			&article.Title,
+			&article.Content,
+			&article.Author,
+			&article.Status,
+			&article.CreatedAt,
+			&article.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		articles = append(articles, article)
+	}
+
+	return articles, nil
+}
+
+func (a *ArticleRepository) GetArticle(ctx context.Context, id uuid.UUID) (*domain.Article, error) {
+	tracer := otel.Tracer("repo.article")
+	ctx, span := tracer.Start(ctx, "ArticleRepository.GetArticle")
+	defer span.End()
+
+	query := `
+		SELECT
+			id,
+			title,
+			content,
+			author,
+			status,
+			created_at,
+			updated_at
+		FROM articles
+		WHERE id = $1 AND deleted_at IS NULL`
+
+	span.SetAttributes(attribute.String("query.statement", query))
+	span.SetAttributes(attribute.String("query.parameter", id.String()))
+	row := a.Conn.QueryRow(ctx, query, id)
+
+	var article domain.Article
+	err := row.Scan(
+		&article.ID,
+		&article.Title,
+		&article.Content,
+		&article.Author,
+		&article.Status,
+		&article.CreatedAt,
+		&article.UpdatedAt,
+	)
+	if err != nil {
+		span.RecordError(err)
+		return nil, err
+	}
+
+	return &article, nil
+}
+
+func (a *ArticleRepository) UpdateArticle(ctx context.Context, id uuid.UUID, article *domain.Article) (*domain.Article, error) {
+	query := `
+		UPDATE articles
+		SET title = $1,
+			content = $2,
+			author = $3,
+			status = $4,
+			updated_at = NOW()
+		WHERE id = $5 AND deleted_at IS NULL`
+
+	_, err := a.Conn.Exec(ctx, query, article.Title, article.Content, article.Author, article.Status, id)
+	if err != nil {
+		return nil, err
+	}
+
+	updatedArticle, err := a.GetArticle(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return updatedArticle, nil
+}
+
+func (a *ArticleRepository) DeleteArticle(ctx context.Context, id uuid.UUID) error {
+	query := `
+		UPDATE articles
+		SET deleted_at = NOW()
+		WHERE id = $1 AND deleted_at IS NULL`
+
+	_, err := a.Conn.Exec(ctx, query, id)
+	return err
+}
---
+++ apps/zog-news/service/article.go
@@ -0,0 +1,116 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"zog-news/domain"
+
+	"github.com/google/uuid"
+	"go.opentelemetry.io/otel"
+)
+
+type ArticleRepository interface {
+	CreateArticle(ctx context.Context, article *domain.CreateArticleRequest) (*domain.Article, error)
+	GetArticleList(ctx context.Context, filter *domain.ArticleFilter) ([]domain.Article, error)
+	GetArticle(ctx context.Context, id uuid.UUID) (*domain.Article, error)
+	UpdateArticle(ctx context.Context, id uuid.UUID, article *domain.Article) (*domain.Article, error)
+	DeleteArticle(ctx context.Context, id uuid.UUID) error
+}
+
+type ArticleService struct {
+	articleRepo ArticleRepository
+}
+
+func NewArticleService(a ArticleRepository) *ArticleService {
+	return &ArticleService{
+		articleRepo: a,
+	}
+}
+
+// CreateArticle adds a new article.
+func (a *ArticleService) CreateArticle(
+	ctx context.Context,
+	u *domain.CreateArticleRequest,
+) (*domain.Article, error) {
+	createdArticle, err := a.articleRepo.CreateArticle(ctx, u)
+	if err != nil {
+		return nil, err
+	}
+	return createdArticle, nil
+}
+
+// GetArticle fetches a article by ID.
+func (a *ArticleService) GetArticle(
+	ctx context.Context,
+	id uuid.UUID,
+) (*domain.Article, error) {
+	tracer := otel.Tracer("service.article")
+	ctxTrace, span := tracer.Start(ctx, "ArticleService.GetArticle")
+	defer span.End()
+
+	article, err := a.articleRepo.GetArticle(ctxTrace, id)
+	if err != nil {
+		return nil, err
+	}
+	return article, nil
+}
+
+// UpdateArticle updates name/email of an existing article.
+func (a *ArticleService) UpdateArticle(
+	ctx context.Context,
+	id uuid.UUID,
+	u *domain.Article,
+) (*domain.Article, error) {
+
+	existing, err := a.articleRepo.GetArticle(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	if existing == nil {
+		return nil, domain.ErrArticleNotFound
+	}
+
+	existing.Title = a.Title
+	existing.Content = a.Content
+	existing.Author = a.Author
+	existing.Status = a.Status
+
+	_, err = a.articleRepo.UpdateArticle(ctx, id, existing)
+	if err != nil {
+		return nil, err
+	}
+
+	return existing, nil
+}
+
+// DeleteArticle removes a article by ID.
+func (a *ArticleService) DeleteArticle(
+	ctx context.Context,
+	id uuid.UUID,
+) error {
+
+	article, err := a.articleRepo.GetArticle(ctx, id)
+	if err != nil {
+		return err
+	}
+	if article == nil {
+		return domain.ErrArticleNotFound
+	}
+
+	err = a.articleRepo.DeleteArticle(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *ArticleService) GetArticleList(ctx context.Context, filter *domain.ArticleFilter) ([]domain.Article, error) {
+	articles, err := a.articleRepo.GetArticleList(ctx, filter)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return articles, nil
+}
